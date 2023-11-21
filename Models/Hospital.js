e838afb12d9507de932097a8acb9f6b2b05fc219
FelipeFreitasDev <135486262+FelipeFreitasDev@users.noreply.github.com>
2023-11-21 18:55:42 -0300

    Adicionado Img ao model de Hospital

--- Models/Hospital.js
+++ Models/Hospital.js
@@ -4,6 +4,7 @@ const HospitalSchema = new mongoose.Schema(
     {
 nameid: {type: String, required:true},
 address: { type: Object, required:true},
+img: { type: String, required: true },
 emails: { type: String, required:true},
 phone: { type: Number, required:true },
     },
