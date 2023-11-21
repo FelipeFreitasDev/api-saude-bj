213509c94617d18bf50b6f10f41fd586db2f2e96
FelipeFreitasDev <135486262+FelipeFreitasDev@users.noreply.github.com>
2023-11-21 19:02:52 -0300

    Adicionando img ao model doctor

--- Models/Doctor.js
+++ Models/Doctor.js
@@ -4,6 +4,7 @@ const DoctorSchema = new mongoose.Schema(
     {
 nameid: {type: String, required:true},
 crm: { type: String, required:true, unique:true},
+img: { type: String, required: true },
 phone: { type: Number, required:true}
 
     });
